cc42dfa988d1ab0daafba4d529e326ba671e1534
igprad01 <igustiputra99@gmail.com>
2024-07-26 10:50:36 +0700

    add operator function

---
+++ R_Programming/day 1/operator.r
@@ -0,0 +1,5 @@
+#case kedua
+print("budi") #menampilkan hasil print
+a = 3 
+b = 3
+print(a * b) #hasil operator dari var a + b
--- R_Programming/day 1/print.r
+++ R_Programming/day 1/print.r
@@ -1,2 +1,11 @@
-print('hello world')
-print(5+6)
+#case pertama
+print('hello world') #menampilkan print hello world
+print(5+6) #operator pada R
+
+#case ketiga case sensitive
+print(01)
+print(1)
+print("01 - 01 - 1980")
+print("1-1-1980")
+print('budi')
+print("BUDI")

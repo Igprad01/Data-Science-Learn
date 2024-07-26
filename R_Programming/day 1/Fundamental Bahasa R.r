39ef6f30a83389bee0b7aa71dbc94554e2413237
igprad01 <igustiputra99@gmail.com>
2024-07-26 11:14:25 +0700

    finish fundamental bab 1 bahasa R

---
+++ R_Programming/day 1/Fundamental Bahasa R.r
@@ -0,0 +1,46 @@
+#case pertama
+print('hello world') #menampilkan print hello world
+print(5+6) #operator pada R
+
+#case ketiga case sensitive
+print(01)
+print(1)
+print("01 - 01 - 1980")
+print("1-1-1980")
+print('budi')
+print("BUDI")
+
+#operator 
+print("budi") #menampilkan hasil print
+a = 3 
+b = 3
+print(a * b) #hasil operator dari var a + b
+
+# #buat function sederhana bahasa R
+ngitungOperator <- function() {
+    a = 5
+    b = 6
+    if (a == b) {
+       print(a + b)
+    } else {
+       print(b + a)
+    }
+}
+ngitungOperator()
+
+#memasukkan numeric ke dalam variable
+budi_berat_kg <- 68
+santi_berat_kg <- 54.5
+pi <- 3.14
+result <- paste(budi_berat_kg, santi_berat_kg, pi)
+
+print(result)
+
+#comment R
+2 + 2 #ini adalah baris komentar
+
+# selesai
+
+
+
+
--- R_Programming/day 1/operator.r
+++
@@ -1,5 +0,0 @@
-#case kedua
-print("budi") #menampilkan hasil print
-a = 3 
-b = 3
-print(a * b) #hasil operator dari var a + b

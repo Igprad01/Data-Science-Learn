82fc9ccdfc9b9dde323f2c77be5a32a2b26ef636
igprad01 <igustiputra99@gmail.com>
2024-07-28 08:06:21 +0700

    on progress tahap2

---
+++ R_Programming/day 1/Vector, List, and Data Frame.r
@@ -0,0 +1,19 @@
+#Vector
+
+# Ini adalah contoh vector untuk angka numerik dengan 3 data c(4, 5, 6)
+c(4,5,6)
+
+b = 23
+
+# Variable bernama angka dengan input berupa vector
+angka <- c(4,5,6)
+# Tampilkan isi variable angka dengan fungsi print
+print(angka)
+
+#deretan nilai operator titik 2:
+angka1 <- c(1,2,3,4,5,6,7,8,9,10)
+print(angka1)
+
+angka2 <- c(1:10)
+print(angka2)
+    

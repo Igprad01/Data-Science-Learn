9522f0d7db990bf6a871151f7eaa60cc8ac02a04
igprad01 <igustiputra99@gmail.com>
2024-07-28 09:27:37 +0700

    index list

--- R_Programming/day 1/Vector, List, and Data Frame.r
+++ R_Programming/day 1/Vector, List, and Data Frame.r
@@ -16,4 +16,75 @@ print(angka1)
 
 angka2 <- c(1:10)
 print(angka2)
-    
+
+#index dan assesor pada vector
+
+# Buat vector variable bernama angka yang isinya 20 s/d 30
+angka <- c(20:30)
+print(angka)
+
+# Tampilkan isi variable angka pada posisi ke 3
+print(angka[3])
+# Tampilkan isi variable angka pada posisi ke 5 gunakan kurung siku ganda
+print(angka[5])
+
+# Tampilkan isi variable angka pada posisi ke 4 s/d 6
+print(angka[4:6])
+# Buat vector teks dengan nama kode_prodi yang diisi sesuai petunjuk soal
+kode_prodi <- c("DKV","ILKOM","ICT")
+
+# Print the modified vector
+print(kode_prodi[3])
+
+#named vector
+
+#Membuat named vector dengan nama nilai
+nilai <- c(statistik = 89, fisika = 95, ilmukomunikasi = 100)
+
+#Menampilkan isi variable nilai
+print(nilai)
+
+#Menampilkan isi dengan nama fisika
+print(nilai["fisika"])
+#Buat variable profil sesuai permintaan soal
+profil <- c(nama = "budi", tempat_tinggal = "jakarta", tingkat_pendidikan = "S1")
+#Tampilkan variable profil
+print(profil)
+
+#list
+# List disimpan dalam variable dengan nama list_random
+list_random <- list(2, "Budi", 4)
+
+# Menampilkan isi list
+list_random 
+
+# List disimpan dalam variable dengan nama dati2
+dati2 <- list(nama = "Denpasar", propinsi = "Bali")
+
+# Menampilkan isi list dati2
+dati2 
+
+# Buat variable kota sesuai permintaan soal
+kota <- list(nama_kota="Makassar", propinsi="Sulawesi Selatan",luas_km2="199.3")
+# Tampilkan isi variable list kota
+kota
+
+#index list
+
+# Membentuk list dengan 2 angka dan 1 character
+list_saya <- list(2, "Budi", 4)
+
+# Menampilkan index kedua dengan aksesor kurung siku tunggal 
+list_saya[2]
+
+# Menampilkan index kedua dengan aksesor kurung siku ganda
+list_saya[[2]]
+
+# Menampilkan index kedua s/d ketiga
+list_saya[2:3]
+
+list_satu <- list(1, "Online", TRUE)
+
+list_satu[[1]]
+
+

b5d2dc7e364fae104a230985291c27c2d356e392
igprad01 <igustiputra99@gmail.com>
2024-07-29 15:10:24 +0700

    add module math

---
+++ Python_Programming/task/modul_and_package.py
@@ -0,0 +1,18 @@
+# import math
+
+import math
+
+print("Nilai pi adalah:",math.pi)
+
+#import math versi fungsi
+
+from math import pi
+
+print("Nilai pi adalah", pi)
+
+#import all moduls math
+
+from math import *
+
+print("Nilai e adalah:", e)
+

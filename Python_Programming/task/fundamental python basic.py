2e9ca351b0be0e1c5812625c660101d449ad569d
igprad01 <igustiputra99@gmail.com>
2024-07-29 13:42:35 +0700

    add modulus

--- Python_Programming/task/fundamental python basic.py
+++ Python_Programming/task/fundamental python basic.py
@@ -21,3 +21,26 @@ if(i < 7):
 		print("nilai i kurang dari 7 dan kurang dari 3")
 	else:
 		print("nilai i kurang dari 7 tapi lebih dari 3")
+  
+#operator python
+a=10
+b=5
+selisih = a-b
+jumlah = a+b
+kali = a*b
+bagi = a/b
+print("Hasil penjumlahan a dan b adalah", jumlah)
+print("Selisih a dan b adalah :",selisih)
+print("Hasil perkalian a dan b adalah :",kali)
+print("Hasil pembagian a dan b adalah:",bagi)
+
+
+#modulus
+
+c = 10
+d = 3
+
+modulus = c%d
+
+print("Hasil modulus",modulus)
+

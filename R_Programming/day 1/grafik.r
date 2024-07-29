809da8e2242381c093df20701c1107adafba0303
igprad01 <igustiputra99@gmail.com>
2024-07-29 13:23:46 +0700

    finish R Programming fundamental

--- R_Programming/day 1/grafik.r
+++ R_Programming/day 1/grafik.r
@@ -60,3 +60,42 @@ print(mahasiswa)
 #Menampilkan kolom Prodi
 print(mahasiswa$Prodi)
 
+
+#pie chart
+library(ggplot2)
+library(openxlsx)
+#Membaca file mahasiswa.xlsx
+mahasiswa <- read.xlsx("https://storage.googleapis.com/dqlab-dataset/mahasiswa.xlsx",sheet = "Sheet 1")
+
+#Menghitung Jumlah Data by Fakultas
+summarybyfakultas <- aggregate(x=mahasiswa$JUMLAH, by=list(Kategori=mahasiswa$Fakultas), FUN=sum)
+summarybyfakultas <- setNames(summarybyfakultas, c("fakultas","jumlah_mahasiswa"))
+
+piechart<- ggplot(summarybyfakultas, aes(x="", y=jumlah_mahasiswa, fill=fakultas))+ geom_bar(width = 1, stat = "identity")
+piechart <- piechart + coord_polar("y", start=0)
+piechart <- piechart + ggtitle("Disribusi Mahasiswa per Fakultas")
+piechart <- piechart + scale_fill_brewer(palette="Blues")+ theme_minimal()
+piechart <- piechart + guides(fill=guide_legend(title="Fakultas"))
+piechart <- piechart + ylab("Jumlah Mahasiswa") 
+piechart
+
+#filtering
+
+library("ggplot2")
+library("openxlsx")
+
+#Membaca file mahasiswa.xlsx
+mahasiswa <- read.xlsx("https://storage.googleapis.com/dqlab-dataset/mahasiswa.xlsx",sheet = "Sheet 1")
+
+#Menghitung Jumlah Data by Fakultas
+summarybyfakultas <- aggregate(x=mahasiswa$JUMLAH, by=list(Kategori=mahasiswa$Fakultas, Tahun=mahasiswa$ANGKATAN), FUN=sum)
+summarybyfakultas <- setNames(summarybyfakultas, c("fakultas","tahun", "jumlah_mahasiswa"))
+summarybyfakultas
+
+summarybyfakultas$tahun = as.factor(summarybyfakultas$tahun)
+summarybyfakultas[summarybyfakultas$fakultas %in% c("ICT", "Ilmu Komunikasi"),]
+
+ggplot(summarybyfakultas[summarybyfakultas$fakultas %in% c("ICT", "Ilmu Komunikasi"),], aes(x=fakultas, y=jumlah_mahasiswa)) + 
+  geom_bar(stat = "identity", aes(fill = tahun), width=0.8, position = position_dodge(width=0.8)) + 
+  theme_classic() 
+

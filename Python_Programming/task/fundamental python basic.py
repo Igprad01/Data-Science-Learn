d6f5b1691399f8dafdddf8a186d0c36efd4a3284
igprad01 <igustiputra99@gmail.com>
2024-07-29 13:30:09 +0700

    add nested IF

--- Python_Programming/task/fundamental python basic.py
+++ Python_Programming/task/fundamental python basic.py
@@ -11,3 +11,13 @@ if (i == 5):
     print("ini adalah angka 5")
 else:
     print("ini adalah bukan angka 5")
+    
+#nested IF
+
+i = 2
+if(i < 7):
+	print("nilai i kurang dari 7")
+	if(i<3):
+		print("nilai i kurang dari 7 dan kurang dari 3")
+	else:
+		print("nilai i kurang dari 7 tapi lebih dari 3")

471d3949b9df08466f74c70148715a7fab20d4d5
igprad01 <igustiputra99@gmail.com>
2024-07-29 15:26:33 +0700

    read data with pandas

--- Python_Programming/task/modul_and_package.py
+++ Python_Programming/task/modul_and_package.py
@@ -16,3 +16,31 @@ from math import *
 
 print("Nilai e adalah:", e)
 
+#membaca teks 
+import requests
+from contextlib import closing
+import csv
+
+# tentukan lokasi file, nama file, dan inisialisasi csv
+url = 'https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv'
+
+# baca file csv secara streaming 
+with closing(requests.get(url, stream=True)) as r:
+    f = (line.decode('utf-8') for line in r.iter_lines())
+
+    reader = csv.reader(f, delimiter=',')
+
+    # membaca baris per baris
+    for row in reader:
+        print(row)
+        
+#membaca data with pandas
+
+import pandas as pd
+
+pd.set_option("display.max_columns", 50)
+
+table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
+
+table.head()
+print(table)

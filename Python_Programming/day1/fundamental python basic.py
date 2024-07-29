b458645c90b5a817e2243c44b1801de8f771f00a
igprad01 <igustiputra99@gmail.com>
2024-07-29 13:22:08 +0700

    add python fundamental

---
+++ Python_Programming/day1/fundamental python basic.py
@@ -0,0 +1,13 @@
+# print dan comment di python
+
+print(10*2+5) #fungsi matematika
+print("Academy DQLab") #fungsi mencetak kalimat
+
+#if else 
+
+i = 5;
+
+if (i == 5):
+    print("ini adalah angka 5")
+else:
+    print("ini adalah bukan angka 5")

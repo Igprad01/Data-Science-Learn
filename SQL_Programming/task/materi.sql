33fe4db1f2d02a9ef8050a114eedfa26f592a277
igprad01 <igustiputra99@gmail.com>
2024-07-30 15:13:02 +0700

    rename dml to materi pada sql

--- SQL_Programming/task/materi.sql
+++ SQL_Programming/task/materi.sql
@@ -12,3 +12,7 @@ SELECT harga, nama_produk FROM ms_produk
 
 -- mengambil data limit pada database
 SELECT harga, nama_produk FROM ms_produk limit 5;
+
+-- meenghilangkan duplikasi data
+SELECT DISTINCT nama_customer,alamat FROM ms_pelanggan;
+

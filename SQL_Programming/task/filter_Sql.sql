561298c3702cd37ea66ace17d47df2a1142c0a28
igprad01 <igustiputra99@gmail.com>
2024-07-30 20:00:56 +0700

    finish course

--- SQL_Programming/task/filter_Sql.sql
+++ SQL_Programming/task/filter_Sql.sql
@@ -9,3 +9,8 @@ SELECT * FROM ms_produk WHERE nama_produk = 'Flashdisk DQLab 64 GB' OR nama_prod
 -- perintah where > 
 SELECT * FROM ms_produk WHERE harga > 50000
 SELECT * FROM ms_produk WHERE nama_produk = 'gantungan Kunci DQLab' AND harga < 50000
+
+
+-- Study kasus akhir
+SELECT 
+    kode_pelanggan,nama_produk,qty,harga, qty * harga AS total FROM  tr_penjualan WHERE   qty * harga >= 100000 ORDER BY total DESC;

ea69e1b7060cf61b64039d4b1c7897f181243f2c
igprad01 <igustiputra99@gmail.com>
2024-07-30 14:46:35 +0700

    add query mengambil kolom pada database

--- SQL_Programming/task/DML.sql
+++ SQL_Programming/task/DML.sql
@@ -1,5 +1,10 @@
--- example
+-- example DML
 
 SELECT * FROM ms_produk
 
+--Mengambil satu kolom pada database
+SELECT nama_produk FROM ms_produk
+
+-- mengambil lebih dari satu kolom pada database 
+SELECT kode_produk, nama_produk FROM ms_produk
 

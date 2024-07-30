bd768fbdd22a26e59330a4136ca91df9cbac0efd
igprad01 <igustiputra99@gmail.com>
2024-07-30 14:56:19 +0700

    add mengambil limit data

--- SQL_Programming/task/DML.sql
+++ SQL_Programming/task/DML.sql
@@ -8,3 +8,7 @@ SELECT nama_produk FROM ms_produk
 -- mengambil lebih dari satu kolom pada database 
 SELECT kode_produk, nama_produk FROM ms_produk
 
+SELECT harga, nama_produk FROM ms_produk
+
+-- mengambil data limit pada database
+SELECT harga, nama_produk FROM ms_produk limit 5;

1e46b199b69c12041414e1d6e71986015be1fdf1
igprad01 <igustiputra99@gmail.com>
2024-07-30 15:56:48 +0700

    add prefik alias table

--- SQL_Programming/task/materi.sql
+++ SQL_Programming/task/materi.sql
@@ -24,3 +24,14 @@ SELECT no_urut AS nomor, nama_produk AS nama FROM ms_produk
 
 -- Mengubah nama kolom tanpa AS 
 SELECT no_urut nomor, nama_produk nama FROM ms_produk;
+
+
+-- gabungkan prefik dan alias
+SELECT ms_produk.harga AS harga_jual fROM ms_produk
+
+-- tanpa alias pada table
+
+SELECT * FROM ms_produk t2
+
+-- prefik alias pada table
+SELECT t2.nama_produk, t2.harga FROM ms_produk t2;
